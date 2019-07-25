384dfedee0fb62219d3ea18972eee854da931f34
bastian <k.batakovich@gmail.com>
2019-07-25 15:23:04 +0300

    Disable CSS modules

--- webpack/rules/css.js
+++ webpack/rules/css.js
@@ -25,8 +25,6 @@ module.exports = {
             loader: 'css-loader',
             options: {
                 importLoaders: 1,
-                modules: true,
-                localIdentName: '[name]__[local]___[hash:base64:5]'
             },
         },
         {
